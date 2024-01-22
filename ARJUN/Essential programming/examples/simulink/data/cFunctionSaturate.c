9ff2f9e5f4f69e4c72c67e41b6cbf2fcbbdcefea
Mallikarjun Yeshlur <127118191+Mallikyesh@users.noreply.github.com>
2024-01-22 21:51:07 +0530

    Deleted

--- ARJUN/Essential programming/examples/simulink/data/cFunctionSaturate.c
+++
@@ -1,9 +0,0 @@
-/* Copyright 2020 The MathWorks, Inc. */
-#include <math.h>
-#include "cFunctionSaturate.h"
-
-/* This function saturates the input value between clipVal and -1*clipVal */
-double customSaturateFcn(double in, double clipVal)  {
-    double outVal = fabs(in) > clipVal ? clipVal : in;
-    return copysign(outVal, in);
-}
